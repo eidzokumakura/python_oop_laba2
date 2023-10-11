267db7ab4067382c2817be7fbaf6a9708d5c9550
Cuoram <106468562+Cuoram@users.noreply.github.com>
2023-10-11 06:50:18 +0300

    37 tasks

--- Задание 22/22.1, 22.2, 22.3, 22.4, 22.5.py
+++ Задание 22/22.1, 22.2, 22.3, 22.4, 22.5.py
@@ -1,21 +1,21 @@
 import re
 
-class Validator():
-    def isEmail(self, email):
+class Validator(): # 22.1 Создание класса Validator
+    def isEmail(self, email): # 22.2
         pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
         if re.search(pattern, email):
             print("Корректный email")
         else:
             print("Некорректный email")
 
-    def isDomain(self, domain):
+    def isDomain(self, domain): # 22.3
         pattern = r'^[a-zA-Z0-9.-]+$'
         if re.search(pattern, domain):
             print("Корректный domain")
         else:
             print("Некорректный domain")
 
-    def isNumber(self, numbers):
+    def isNumber(self, numbers): # 22.4
         if numbers.isdigit():
             print("Только цифры")
         else:
